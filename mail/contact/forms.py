ae996324155a6f0ee5ed4f8e0833f0db68ed3b2e
Abhisek kumar singh <87076425+Denver04@users.noreply.github.com>
2023-06-08 15:59:52 +0530

    added mail django

--- mail/contact/forms.py
+++ mail/contact/forms.py
@@ -3,44 +3,6 @@ from django.conf import settings
 from django.core.mail import send_mail
 from .models import *
 
-# class ContactForm(forms.Form):
-
-#     name = forms.CharField(max_length=120)
-#     email = forms.EmailField()
-#     inquiry = forms.CharField(max_length=70)
-#     message = forms.CharField(widget=forms.Textarea)
-
-#     def get_info(self):
-#         """
-#         Method that returns formatted information
-#         :return: subject, msg
-#         """
-#         # Cleaned data
-#         cl_data = super().clean()
-
-#         name = cl_data.get('name').strip()
-#         from_email = cl_data.get('email')
-#         subject = cl_data.get('inquiry')
-
-#         msg = f'{name} with email {from_email} said:'
-#         msg += f'\n"{subject}"\n\n'
-#         msg += cl_data.get('message')
-
-#         return subject, msg
-
-#     def send(self):
-
-#         subject, msg = self.get_info()
-
-#         send_mail(
-#             subject=subject,
-#             message=msg,
-#             from_email=settings.EMAIL_HOST_USER,
-#             recipient_list=[settings.RECIPIENT_ADDRESS]
-#         )
-
-
-
 class ContactForm(forms.ModelForm):
 	class Meta:
 		model = ContactProfile
@@ -57,7 +19,7 @@ class ContactForm(forms.ModelForm):
 				border-bottom: 1px solid grey; \
 				outline: none; \
 				font-size: 18px; \
-				background-color: #a7c9de;"}),
+				background-color: #c5ccd0;"}),
 
    
 			'email': forms.TextInput(attrs={'class': 'form-control', 'style':
@@ -66,7 +28,7 @@ class ContactForm(forms.ModelForm):
 				border-bottom: 1px solid grey; \
 				outline: none; \
 				font-size: 18px; \
-				background-color: #a7c9de;"}),
+				background-color: #c5ccd0;"}),
    
 			'message': forms.Textarea(attrs={'class': 'form-control', 'style':
 				"padding: 10px; \
@@ -74,7 +36,7 @@ class ContactForm(forms.ModelForm):
 				border-bottom: 1px solid grey; \
 				outline: none; \
 				font-size: 18px; \
-				background-color: #a7c9de;"}),
+				background-color: #c5ccd0;"}),
    
 			'subject': forms.TextInput(attrs={'class': 'form-control', 'style':
 				"padding: 10px; \
@@ -82,5 +44,5 @@ class ContactForm(forms.ModelForm):
 				border-bottom: 1px solid grey; \
 				outline: none; \
 				font-size: 18px; \
-				background-color: #a7c9de;"}),
+				background-color: #c5ccd0;"}),
 		}
